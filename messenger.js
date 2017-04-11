db3ce7cccbe181b0871f2840251e4bbedfb9fdd7
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2017-04-10 20:11:42 -0700

    cleanup remove config dep

--- messenger.js
+++ messenger.js
@@ -1,26 +1,25 @@
 "use strict";
 
 const
-    config = require('config'),
     crypto = require('crypto'),
     request = require('request');
 
-function Messenger() {
+function Messenger(config) {
     const appSecret = (process.env.MESSENGER_APP_SECRET) ?
         process.env.MESSENGER_APP_SECRET :
-        config.get('appSecret');
+        config.appSecret;
 
     const validationToken = (process.env.MESSENGER_VALIDATION_TOKEN) ?
         (process.env.MESSENGER_VALIDATION_TOKEN) :
-        config.get('validationToken');
+        config.validationToken;
 
     const pageAccessToken = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
         (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-        config.get('pageAccessToken');
+        config.pageAccessToken;
 
     const logApi = (process.env.LOG_API) ?
         (process.env.LOG_API) :
-        config.get('logAPI');
+        config.logAPI;
 
     if (!(appSecret && validationToken && pageAccessToken)) {
         console.error("Missing config values");
