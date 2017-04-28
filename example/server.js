4f258229296dd428954a5709a4d6cca76d9b0ffb
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2017-04-27 19:26:04 -0700

    profile sample

--- example/server.js
+++ example/server.js
@@ -9,26 +9,39 @@ var verifySignature = true;
 var messengerapi = require('../node-facebook-messenger-api').messenger();
 var messenger = new messengerapi.Messenger({});
 var webhookHandler = require('../node-facebook-messenger-api').webhookHandler()(messenger, {
-  receivedAuthentication : function(event) {
+  receivedAuthentication: function (event) {
     console.log('receivedAuthentication', event);
   },
-  handleMessage : function(event) {
+  handleMessage: function (event) {
     console.log('handleMessage', event);
-    messenger.sendTextMessage(event.sender.id, JSON.stringify(event));
+    if (event.message && event.message.text) {
+      switch (event.message.text) {
+        case 'profile':
+          messenger.getUserProfile(event.sender.id, (err, resp) => {
+            messenger.sendTextMessage(event.sender.id, JSON.stringify(err ? err : resp));
+          });
+          break;
+        default:
+          messenger.sendTextMessage(event.sender.id, JSON.stringify(event));
+          break;
+      }
+    } else {
+      messenger.sendTextMessage(event.sender.id, JSON.stringify(event));
+    }
   },
-  receivedDeliveryConfirmation : function(event) {
+  receivedDeliveryConfirmation: function (event) {
     console.log('receivedDeliveryConfirmation', event);
   },
-  receivedPostback : function(event) {
+  receivedPostback: function (event) {
     console.log('receivedPostback', event);
   },
-  receivedMessageRead : function(event) {
+  receivedMessageRead: function (event) {
     console.log('receivedMessageRead', event);
   },
-  receivedAccountLink : function(event) {
+  receivedAccountLink: function (event) {
     console.log('receivedAccountLink', event);
   }
-},verifySignature, ignores, express.Router());
+}, verifySignature, ignores, express.Router());
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
